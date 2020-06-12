64d3c4d6f4f7ae9759bbff8ff9666d764687dffa
dalysmedia <dalyjjjk@gmail.com>
2020-06-13 00:58:26 +0530

    update this

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-         abcd
+          Dalys Media Comming Soon ...
         </h1>
       </header>
     </div>
